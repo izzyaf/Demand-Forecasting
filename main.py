080fd94023dd48ec5a84c417a8a69956804ea1a9
Le Minh Pham Hoang <idoraemon0121@gmail.com>
2016-06-26 21:44:25 +0700

    updated: data sources, input parsing

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
+# Import
+
+from __future__ import print_function
 import pandas as pd
+import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
-import preprocessing as pre
-
-matplotlib.style.use("ggplot")
-
-rng = pd.period_range("2014-12", "2017-12", freq="M")
-forecastAllItem = pd.DataFrame(index=rng, columns=["Total orders"])
-forecastAllItem.update(pre.dfAllItems)
-forecastAllItem = pd.ewma(forecastAllItem, span=3)
-print(forecastAllItem)
-
-forecastAllItem.plot(ax=pre.dfAllItems.plot())
-plt.show()
 
 
+# ------------------------------------------------------------------
 
+# Process .csv as input. Return a dataframe, with time-series used as index
+
+
+def parse_csv_file(file_name, date_format):
+    data = pd.read_csv(filepath_or_buffer=file_name, parse_dates=True, date_parser=date_format, index_col=0,
+                       squeeze=True)
+    return data
+
+
+# ------------------------------------------------------------------
+
+# Generate historical dataframe
+
+file_name = 'data/car.csv'
+f = open('data/raw_historical_data.txt', 'w')
+date_format = lambda dates: pd.datetime.strptime(dates, "%Y-%m")
+
+historical_data = parse_csv_file(file_name, date_format)
+
+print(historical_data, file=f)
+f.close()
+
+# ------------------------------------------------------------------
